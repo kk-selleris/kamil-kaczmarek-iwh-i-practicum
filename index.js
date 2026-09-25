e60cf5bc6098aadff98871553edf8da58da8319b
Kamil Kaczmarek <kk@selleris.com>
2026-09-25 12:57:51 +0200

    Add dotenv dependency and load token from .env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -8,7 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+if (!PRIVATE_APP_ACCESS) {
+    console.error('Missing PRIVATE_APP_ACCESS in .env');
+    process.exit(1);
+}
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
